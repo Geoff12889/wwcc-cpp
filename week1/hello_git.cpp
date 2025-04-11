d67ddb0a4260e2430b4f9eea251bbc7b5b396e4f
Geoffrey Woytt <geoffreywoytt@Geoffreys-MacBook-Pro-935.local>
2025-04-10 18:28:44 -0700

    Fixes error of undeclared identifier 'name'. --GW

--- week1/hello_git.cpp
+++ week1/hello_git.cpp
@@ -2,6 +2,7 @@
 using namespace std;
 // main: generate some simple output
 int main () {
+  string name;
   cout << "Enter your name: ";
   cin >> name;
   cout << "Hello, " << name << "!" << endl;
