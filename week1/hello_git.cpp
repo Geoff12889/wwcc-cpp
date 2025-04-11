d4406291804f9497a0d1e1911a450d4e045c9955
Geoffrey Woytt <geoffreywoytt@Geoffreys-MacBook-Pro-935.local>
2025-04-10 18:27:01 -0700

    Changes hello_git.cpp to read name inputed by the user. --GW

--- week1/hello_git.cpp
+++ week1/hello_git.cpp
@@ -2,6 +2,8 @@
 using namespace std;
 // main: generate some simple output
 int main () {
-  cout << "Hello, World." << endl;
+  cout << "Enter your name: ";
+  cin >> name;
+  cout << "Hello, " << name << "!" << endl;
   return 0;
 }
