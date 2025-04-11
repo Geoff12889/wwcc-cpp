ee8d1db0031e53d02f96ac2e59574004cd59e5e7
Geoffrey Woytt <geoffreywoytt@Geoffreys-MacBook-Pro-935.local>
2025-04-10 18:35:48 -0700

    Adds compiled version of hello_git with full name input. --GW

--- week1/hello_git.cpp
+++ week1/hello_git.cpp
@@ -2,9 +2,9 @@
 using namespace std;
 // main: generate some simple output
 int main () {
-  string name;
-  cout << "Enter your name: ";
-  cin >> name;
-  cout << "Hello, " << name << "!" << endl;
+  string fullName;
+  cout << "Enter your full name: ";
+  getline(cin, fullName);
+  cout << "Hello, " << fullName << "!" << endl;
   return 0;
 }
